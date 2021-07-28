688523e5f1b78b4874e4f0a3348e8c4bb3a0485c
nadeem1108 <nadeemcoc8@gmail.com>
2021-07-28 18:52:22 +0530

    multer done

--- api/users.js
+++ api/users.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var uploadMultiple = require("../controller/multer/multer");
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.render("index");
+});
+
+router.post("/uploadfile", uploadMultiple, async (req, res, next) => {
+  
+  if (req.files) {
+    console.log(req.body.url);
+   console.log(req.files);
+    console.log("files uploaded");
+  }
 });
 
 module.exports = router;
--- controller/products/products.js
+++ controller/products/products.js
@@ -1,37 +1,34 @@
-const Product = require("../../modules/products/product")
+const Product = require("../../modules/products/product");
 
+module.exports = (req, res, next) => {
+  var { name, src, des, subDes, price, discountPrice, size, qty } = req.body;
 
-
-module.exports=(req, res, next)=>{
-    var {name,src,des,subDes,price,discountPrice,size,qty,}= req.body;  
-        
-            var product = new Product({
-                
-                name:name,
-                src:src,
-                des:des,
-                subDes:subDes,
-                price:price,
-                discountPrice:discountPrice,
-                size:size,
-                qty:qty,
-                createdDate:new Date(),
-                status:1
-                
-              });
-              product.save()
-            .then((result)=>{
-                res.status(201).json({
-                    msg:"SUCCESS",
-                    result:result
-                });
-            })
-            .catch((err)=>{
-                res.json({
-                    msg:"FAIL",
-                    err:err,
-                })
-            })
-        }
-
-
+  var product = new Product({
+    name: name,
+    src: {
+      originalname: originalname,
+    },
+    des: des,
+    subDes: subDes,
+    price: price,
+    discountPrice: discountPrice,
+    size: size,
+    qty: qty,
+    createdDate: new Date(),
+    status: 1,
+  });
+  product
+    .save()
+    .then((result) => {
+      res.status(201).json({
+        msg: "SUCCESS",
+        result: result,
+      });
+    })
+    .catch((err) => {
+      res.json({
+        msg: "FAIL",
+        err: err,
+      });
+    });
+};
